0646555a6accdea8f90e3f888207e1b00d3d632c
Jechau2 <117667268+Jechau2@users.noreply.github.com>
2025-06-22 01:52:53 +0200

    Ajout systeme de sauvegarde

---
+++ include/character.h
@@ -0,0 +1,19 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "inventory.h"
+#include <string>
+
+struct Character {
+    std::string nom{"H\xC3\xA9ros"};
+    int niveau{1};
+    int pv{100};
+    int pm{50};
+    int force{10};
+    int defense{10};
+    int agilite{10};
+    int intelligence{10};
+    Inventory inventaire;
+};
+
+#endif // CHARACTER_H
